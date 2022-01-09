544141f6570de1db78ac6c0a60f7fc78ab8de228
Filipe <filipeandrade000@gmail.com>
2022-01-09 01:52:24 -0300

    Adicionado Programa2

---
+++ Programa2/Programa2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeirosProgramasCSharp
+{
+    class Program1{
+
+        static void Main(string[] args){
+           
+            int idade = 0, quantidade=0;
+            int i = 1;
+            Console.WriteLine("Programa que calcula a idade media de cinco alunos");
+            while(i<10){
+               
+               Console.Write("Informe a idade da "+i+"°"+" pessoa : ");
+               idade = Convert.ToInt32(Console.ReadLine());
+               if (idade>18){
+
+                   quantidade = quantidade + 1; 
+               }
+               ++i;
+            }
+             Console.WriteLine("A quantidade de pessoas de maior idade é " + quantidade);         
+
+
+        }
+    }
+}
